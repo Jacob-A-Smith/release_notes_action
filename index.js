053645874448f5454fd1ad25d0a7decac35107f2
Jacob Smith <jsmith@sparkfly.com>
2020-07-23 14:40:01 -0400

    trying spawn instead

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { execSync } = require('child_process');
+const { spawn } = require('child_process');
 const { stderr, stdout } = require('process');
 const fs = require('fs')
 
@@ -9,7 +9,7 @@ console.log('Entering release notes generator')
 try {
     console.log(`Path to file: ${core.getInput('path-to-file')}`);
 
-    const stdout = execSync('git log --pretty=format:"{"hash": "%h", "author": "%an", "email": "%ae", "date": "%ad", "message": "%B"}*" --all').toString()//, (error, stdout, stderr) => {
+    // const stdout = execSync('git log --pretty=format:"{"hash": "%h", "author": "%an", "email": "%ae", "date": "%ad", "message": "%B"}*" --all').toString()//, (error, stdout, stderr) => {
     /*    if (error) {
             console.log(`exec error: ${error}`)
             core.setFailed(error.mes-sage);
@@ -25,9 +25,25 @@ try {
             if (error) throw error;
             console.log('SAVED!');
         });*/
-    let commits = stdout.split("*");
-    commits.forEach(msg => {
-        console.log(msg)
+    // let commits = stdout.split("*");
+    // commits.forEach(msg => {
+    // console.log(msg)
+    // });
+    let git = spawn('git', ['log', '--pretty=format:"{"hash": "%h", "author": "%an", "email": "%ae", "date": "%ad", "message": "%B"}'])
+    let buf = Buffer.alloc(0)
+    git.stdout.on('data', (data) => {
+        buf = Buffer.concat([buf, data])
+    });
+
+    git.stderr.on('data', (data) => {
+        console.log(data.toString());
+    });
+
+    git.on('close', (code) => {
+        let commits = buf.toString().split('\n');
+        commits.forEach(msg => {
+            console.log(msg)
+        });
     });
 } catch (error) {
     core.setFailed(error.message);
