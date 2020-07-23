7f1d3f69426a178e3954f593541e9a03ec7135fe
Jacob Smith <jsmith@sparkfly.com>
2020-07-23 15:14:48 -0400

    get commit info

--- index.js
+++ index.js
@@ -1,51 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { spawn } = require('child_process');
-const { stderr, stdout } = require('process');
-const fs = require('fs')
+// const { spawn } = require('child_process');
+// const { stderr, stdout } = require('process');
+// const fs = require('fs')
 
 console.log('Entering release notes generator')
 
 try {
     console.log(`Path to file: ${core.getInput('path-to-file')}`);
-    console.log(`The event payload: ${JSON.stringify(github.context.payload, undefined, 2)}`)
+    // console.log(`The event payload: ${JSON.stringify(github.context.payload, undefined, 2)}`)
 
-    // const stdout = execSync('git log --pretty=format:"{"hash": "%h", "author": "%an", "email": "%ae", "date": "%ad", "message": "%B"}" --all').toString()//, (error, stdout, stderr) => {
-    /*    if (error) {
-            console.log(`exec error: ${error}`)
-            core.setFailed(error.mes-sage);
-        }
-        // console.log(`stdout: ${stdout.toString()}`)
-        let strs = stdout.toString().split("}")
-        strs.forEach((msg) => {
-            console.log(msg)
-        });
-        // console.error(`stderr: ${stderr}`)
+    const payload = JSON.stringify(github.context.payload)
 
-        /*fs.writeFile('release.txt', stdout, (error) => {
-            if (error) throw error;
-            console.log('SAVED!');
-        });*/
-    // let commits = stdout.split("*");
-    // commits.forEach(msg => {
-    // console.log(msg)
-    // });
-    let git = spawn('git', ['log', '--pretty=format:"{"hash": "%h", "author": "%an", "email": "%ae", "date": "%ad", "message": "%B"}'])
-    let buf = Buffer.alloc(0)
-    git.stdout.on('data', (data) => {
-        buf = Buffer.concat([buf, data])
-    });
+    entry = {
+        "author": payload.head_commit.author.name,
+        "email": payload.head_commit.author.email,
+        "message": payload.head_commit.message,
+        "date": payload.head_commit.timestamp,
+        "git url": payload.head_commit.url
+    }
+    console.log(`Commit: ${entry}`);
 
-    git.stderr.on('data', (data) => {
-        console.log(data.toString());
-    });
 
-    git.on('close', (code) => {
-        let commits = buf.toString().split('\n');
-        commits.forEach(msg => {
-            console.log(msg)
-        });
-    });
 } catch (error) {
     core.setFailed(error.message);
 }
