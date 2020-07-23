875a405b4a77e6f0bf03dca3a1d4a1ea909128c5
Jacob Smith <jsmith@sparkfly.com>
2020-07-23 13:51:20 -0400

    try something different

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { exec } = require('child_process');
+const { stderr } = require('process');
 
 console.log('Entering release notes generator')
 
 try {
     console.log(`Path to file: ${core.getInput('path-to-file')}`);
+
+    exec('git log --pretty=format:"{%h, %t, %p  - %an, %ae, %ad: %B}" > log.txt', (error, stdout, stderr) => {
+        if (error) {
+            console.log(`exec error: ${error}`)
+            core.setFailed(error.message);
+        }
+        console.log(`stdout: ${stdout}`)
+        console.error(`stderr: ${stderr}`)
+    });
 } catch (error) {
     core.setFailed(error.message);
 }
