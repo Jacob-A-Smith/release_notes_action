4eed09b53062534dac31ebb1bb962cc37f7d897b
Jacob Smith <jsmith@sparkfly.com>
2020-07-24 15:29:47 -0400

    update read/write

--- actions/release_notes_generator/index.js
+++ actions/release_notes_generator/index.js
@@ -3,41 +3,41 @@ const github = require('@actions/github');
 // const { spawn } = require('child_process');
 // const { stderr, stdout } = require('process');
 const fs = require('fs');
-const { stringify } = require('querystring');
 
 console.log('Entering release notes generator')
 
 try {
-    // console.log(`Path to file: ${core.getInput('path-to-file')}`);
-    console.log(`The event payload: ${JSON.stringify(github.context.payload, undefined, 2)}`)
-    // console.log(`REF: ${core.getInput('branch-name')}`);
-
     const payload = github.context.payload
-    const pathToFile = core.getInput('path-to-file');
-    // const destination = core.getInput('destination-branch');
-    // const base = core.getInput('base-branch');
-    const branch = core.getInput('branch-name');
-    console.log(`BRANCH NAME: ${branch}`);
-    // const master = core.getInput('master-branch');
-    // const staging = core.getInput('staging-branch')
-    // const develop = core.getInput('develop-branch')
+    console.log(`The event payload: ${JSON.stringify(github.context.payload, undefined, 2)}`)
 
-    // let refs = stringify(github.context.payload.ref).split("/")[2] // ?
-    // console.log(`REFS: Master: ${master} - Staging: ${staging} - Develop: ${develop}`);
+    const pathToFile = core.getInput('path-to-file');
+    const branch = core.getInput('branch-name');
+    console.log(`BRANCH NAME: ${branch} --- PATH TO FILE: ${pathToFile}`);
+
 
     entry = {
         "author": payload.head_commit.author.name,
         "email": payload.head_commit.author.email,
         "message": payload.head_commit.message,
         "date": payload.head_commit.timestamp,
-        "git url": payload.head_commit.url
+        "git url": payload.head_commit.url,
+        "summary": getSummary(payload.commits)
     }
     console.log(`Commit: ${JSON.stringify(entry, undefined, 2)}`);
 
-    /*let prevCommits = JSON.parse(fs.readFileSync(pathToFile, 'utf8').toString(), undefined, 2);
+    let prevCommits = JSON.parse(fs.readFileSync(pathToFile, 'utf8').toString(), undefined, 2) || { "history": [] };
+    // prevCommits = prevCommits || { "history": [] };
+    // if (prevCommits === undefined) {
+    // prevCommits = {
+    // "history": []
+    // }
+    // }
+
+    // if (!())
+    prevCommits["history"].push(entry)
 
     // payload.base_ref
-    switch (payload.ref) {
+    /*switch (payload.ref) {
         case master:
             console.log("MASTER")
             appendToMaster(entry, prevCommits, "master")
@@ -54,16 +54,17 @@ try {
             console.log("DEFAULT")
             appendToDevelop(entry, prevCommits, payload.ref)
             break;
-    }
+    }*/
 
     fs.writeFile(pathToFile, JSON.stringify(prevCommits, undefined, 2), err => {
         if (err) throw err;
         console.log("done writing");
-    });*/
+    });
+
 } catch (error) {
     core.setFailed(error.message);
 }
-
+/*
 function appendToMaster(commit, prevCommits, branchRef) {
     if (!(branchRef in prevCommits)) {
         prevCommits[branchRef] = []
@@ -83,4 +84,12 @@ function appendToDevelop(commit, prevCommits, branchRef) {
         prevCommits[branchRef] = []
     }
     prevCommits[branchRef].push(commit)
+}
+*/
+
+function getSummary(commits) {
+    console.log("Reduce commits")
+    return commits.reduce((total, currentVal) => {
+        return total += currentVal.message + ", "
+    })
 }
