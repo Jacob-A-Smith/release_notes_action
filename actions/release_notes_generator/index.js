f2cc245dac68e6c6dcfeadd13c8a45d789c050c5
Jacob Smith <jsmith@sparkfly.com>
2020-07-25 12:10:01 -0400

    code cleanup

--- actions/release_notes_generator/index.js
+++ actions/release_notes_generator/index.js
@@ -5,21 +5,13 @@ const fs = require('fs');
 console.log('Entering release notes generator')
 
 try {
-    const payload = github.context.payload
-    console.log(`The event payload: ${JSON.stringify(github.context.payload, undefined, 2)}`)
+    const payload = github.context.payload;
+    // console.log(`The event payload: ${JSON.stringify(github.context.payload, undefined, 2)}`);
 
     const pathToFile = core.getInput('path-to-file');
     const branch = core.getInput('branch-name');
     console.log(`BRANCH NAME: ${branch} --- PATH TO FILE: ${pathToFile}`);
 
-
-    /* entry = {
-         "author": payload.head_commit.author.name,
-         "email": payload.head_commit.author.email,
-         "message": payload.head_commit.message,
-         "date": payload.head_commit.timestamp,
-         "git url": payload.head_commit.url
-     }*/
     let entries = [];
     for (i = 0; i < payload.commits.length; ++i) {
         entries.push({
@@ -30,16 +22,11 @@ try {
             "gitURL": payload.commits[i].url
         });
     }
-    console.log(`Commits: ${JSON.stringify(entries, undefined, 2)}`);
+    // console.log(`Commits: ${JSON.stringify(entries, undefined, 2)}`);
 
     let history = readPrevCommits(pathToFile);
-    // history.history = history.history.concat(entries)
-    // let writeDate = {
-    // "history": [...prevCommits.history, ...entries]
-    // }
-    // console.log(`WRITE DATA: \n${JSON.stringify(writeDate, undefined, 2)}`);
-    history.history.push(entries)
-    console.log(`PREVIOUS COMMITS: \n${JSON.stringify(history, undefined, 2)}`);
+    history.entries.push(entries)
+    // console.log(`PREVIOUS COMMITS: \n${JSON.stringify(history, undefined, 2)}`);
 
     fs.writeFile(pathToFile, JSON.stringify(history, undefined, 2), err => {
         if (err) throw err;
@@ -52,7 +39,7 @@ try {
 
 function readPrevCommits(filepath) {
     let data = fs.readFileSync(filepath, 'utf8').toString();
-    console.log(`READ DATA: \n${data}`);
+    // console.log(`READ DATA: \n${data}`);
     if (data) {
         try {
             let prevCommits = JSON.parse(data);
@@ -61,12 +48,5 @@ function readPrevCommits(filepath) {
             console.log(`error parsing: ${e}`);
         }
     }
-    return { "history": [] };
+    return { "entries": [] };
 }
-/*
-function getSummary(commits) {
-    console.log("Reduce commits")
-    return commits.reduce((total, currentVal) => {
-        return total += currentVal.message + ", "
-    })
-}*/
