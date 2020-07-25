4bedc074490f336997820d223515607552848e4d
Jacob Smith <jsmith@sparkfly.com>
2020-07-25 12:59:45 -0400

    error in log file, reorganize index.js

--- actions/release_notes_generator/index.js
+++ actions/release_notes_generator/index.js
@@ -2,51 +2,70 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
 
-console.log('Entering release notes generator')
-
-try {
-    const payload = github.context.payload;
-    // console.log(`The event payload: ${JSON.stringify(github.context.payload, undefined, 2)}`);
-
-    const pathToFile = core.getInput('path-to-file');
-    const branch = core.getInput('branch-name');
-    console.log(`BRANCH NAME: ${branch} --- PATH TO FILE: ${pathToFile}`);
-
-    let entries = [];
-    for (i = 0; i < payload.commits.length; ++i) {
-        entries.push({
-            "author": payload.commits[i].author.name,
-            "email": payload.commits[i].author.email,
-            "message": payload.commits[i].message,
-            "date": payload.commits[i].timestamp,
-            "gitURL": payload.commits[i].url
-        });
-    }
-    // console.log(`Commits: ${JSON.stringify(entries, undefined, 2)}`);
-
-    let history = readPrevCommits(pathToFile);
-    history.entries.push(entries)
-    // console.log(`PREVIOUS COMMITS: \n${JSON.stringify(history, undefined, 2)}`);
-
-    fs.writeFile(pathToFile, JSON.stringify(history, undefined, 2), err => {
-        if (err) throw err;
-        console.log("done writing");
-    });
-
-} catch (error) {
-    core.setFailed(error.message);
-}
-
 function readPrevCommits(filepath) {
     let data = fs.readFileSync(filepath, 'utf8').toString();
-    // console.log(`READ DATA: \n${data}`);
     if (data) {
         try {
             let prevCommits = JSON.parse(data);
             return prevCommits;
         } catch (e) {
-            console.log(`error parsing: ${e}`);
+            console.log(`error parsing log file: ${e}`);
         }
     }
     return { "entries": [] };
+}
+
+function parseCommits(commitPayload) {
+    let commitData = [];
+    for (i = 0; i < commitPayload.length; ++i) {
+        entries.push({
+            "author": commitPayload[i].author.name,
+            "email": commitPayload[i].author.email,
+            "message": commitPayload[i].message,
+            "date": commitPayload[i].timestamp,
+            "gitURL": commitPayload[i].url
+        });
+    }
+    return commitData
+}
+
+console.log('Entering release notes generator')
+try {
+    // SETUP //
+    const payload = github.context.payload;
+    const pathToFile = core.getInput('path-to-file');
+    const branch = core.getInput('branch-name');
+    console.log(`BRANCH NAME: ${branch} --- PATH TO FILE: ${pathToFile}`);
+    // console.log(`The event payload: ${JSON.stringify(github.context.payload, undefined, 2)}`);
+
+    // PARSE GIT DATA //
+    let entries = parseCommits(payload.commits)
+    console.log(`Payload parsed, ${entries.length} new commit(s) detected`)
+    // console.log(`COMMITS: ${JSON.stringify(entries, undefined, 2)}`);
+
+    // READ IN PREVIOUS ENTRIES //
+    let history = readPrevCommits(pathToFile);
+    console.log(`Commit history parsed, ${history.entries.length} previous entries read`);
+    // console.log(`PREVIOUS ENTRIES: \n${JSON.stringify(history, undefined, 2)}`);
+
+    // APPEND NEW DATA TO OLD //
+    history.entries.push(entries);
+    console.log(`Added new entry to log file, ${history.entries.length} entries total`);
+    // console.log(`UPDATED ENTRIES: \n${JSON.stringify(history, undefined, 2)}`);
+
+    // WRITE DATA & EXIT //
+    fs.writeFile(filepath, JSON.stringify(data, undefined, 2), err => {
+        if (err) {
+            throw err;
+        }
+        console.log(`done logging commit(s) to file: ${filepath}`);
+        console.log('Exiting release notes generator safely');
+    });
+} catch (error) {
+    // NOTE:
+    // should probably do something if there is an error with
+    // pushing new entries to the file
+
+    console.log(`Exception generating release notes: ${error}`);
+    core.setFailed(error.message);
 }
